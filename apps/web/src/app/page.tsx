c269188b9a97b7cfee097bc7390c7f6ed1f41d89
Benjamin Kofi Sekyi <benjaminkofi.sekyi@gmail.com>
2026-09-10 10:37:07 +0000

    feat; Dynamic tab title

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,17 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DocLift - Convert",
+};
 
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">
-                {/* Your page content */}
+                {/* page content */}
             </main>
 
             <Footer />
