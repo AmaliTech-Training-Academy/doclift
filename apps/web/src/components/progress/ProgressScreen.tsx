cb47030a6e723b25315c08bfe528fad7fa41d1df
pastelDev3985 <nanamarfo860@gmail.com>
2026-09-17 13:16:19 +0000

    feat: add ProgressScreen component with HeaderBar and ProgressCard and wire SPA flow

---
+++ apps/web/src/components/progress/ProgressScreen.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import ProgressCard from "./ProgressCard";
+import HeaderBar from "./HeaderBar";
+import { useConversion } from "@/context/ConversionContext";
+import ErrorStateCard from "./ErrorStateCard";
+import error from "next/dist/api/error";
+
+const ProgressScreen = () => {
+  const { file } = useConversion();
+
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(0);
+
+  return (
+    <div>
+      <HeaderBar
+        file={file}
+        timeElapsed={timeElapsed}
+        timeRemaining={timeRemaining}
+      />
+      <ProgressCard />
+      
+    </div>
+  );
+};
+
+export default ProgressScreen;
