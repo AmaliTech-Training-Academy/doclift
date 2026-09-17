83df6c727eb11a172518b9e0a34fd7e2fcfc9d09
Willard-Henry <henrynev04@gmail.com>
2026-09-17 11:52:22 +0000

    refactor: version upload API endpoint

--- apps/backend/src/test/java/com/amalitech/backend/controller/UploadControllerTest.java
+++ apps/backend/src/test/java/com/amalitech/backend/controller/UploadControllerTest.java
@@ -45,7 +45,7 @@ class UploadControllerTest {
                 .thenReturn(job);
 
         mockMvc.perform(
-                        multipart("/upload")
+                        multipart("/api/v1/upload")
                                 .file(file)
                 )
                 .andExpect(status().isCreated())
@@ -72,7 +72,7 @@ class UploadControllerTest {
                 ));
 
         mockMvc.perform(
-                        multipart("/upload")
+                        multipart("/api/v1/upload")
                                 .file(file)
                 )
                 .andExpect(status().isBadRequest())
@@ -101,7 +101,7 @@ class UploadControllerTest {
                 ));
 
         mockMvc.perform(
-                        multipart("/upload")
+                        multipart("/api/v1/upload")
                                 .file(file)
                 )
                 .andExpect(status().isBadRequest())
@@ -130,7 +130,7 @@ class UploadControllerTest {
                 ));
 
         mockMvc.perform(
-                        multipart("/upload")
+                        multipart("/api/v1/upload")
                                 .file(file)
                 )
                 .andExpect(status().isPayloadTooLarge())
