592c77c0c316e1be9afc6785f9371e709ba6c9cd
enkadi <adiemmanuel670@gmail.com>
2026-09-18 16:16:02 -0700

    Test CI on feature branch

---
+++ apps/backend/src/main/java/com/amalitech/backend/service/interfaces/PdfExtractionService.java
@@ -0,0 +1,16 @@
+package com.amalitech.backend.service.interfaces;
+
+import com.amalitech.backend.service.PdfExtractionResult;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface PdfExtractionService {
+
+    PdfExtractionResult extract(byte[] pdfBytes) throws IOException;
+
+    PdfExtractionResult extract(InputStream inputStream) throws IOException;
+
+    PdfExtractionResult extract(PDDocument document) throws IOException;
+}
