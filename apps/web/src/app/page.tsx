246783c6775cb3e555aa7b6a845a90854262ec84
pastelDev3985 <nanamarfo860@gmail.com>
2026-09-23 08:27:08 +0000

    chore: remove unused favicon and integrate new icon asset

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,8 +4,6 @@ import UploadScreen from "@/components/upload/UploadScreen";
 import ProgressScreen from "@/components/progress/ProgressScreen";
 import ResultsScreen from "@/components/results/ResultsScreen";
 import { useConversion } from "@/context/ConversionContext";
-import ErrorStateCard from "@/components/progress/ErrorStateCard";
-import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 export default function Home() {
   const { resetKey, activeView } = useConversion();
@@ -13,12 +11,12 @@ export default function Home() {
   const { reset } = useConversion();
 
   return (
-    // <ErrorBoundary fallback={ErrorStateCard}>
+  
     <main className="min-h-dvh flex flex-col">
       {activeView === "upload" && <UploadScreen key={resetKey} />}
       {activeView === "progress" && <ProgressScreen />}
       {activeView === "result" && <ResultsScreen />}
     </main>
-    // </ErrorBoundary>
+  
   );
 }
